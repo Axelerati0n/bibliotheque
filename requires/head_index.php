1a8597ad3b3ac5a139b3de8706c6db00304e09de
A1exTerieur <76064077+A1exTerieur@users.noreply.github.com>
2021-01-21 16:52:00 +0100

    Add files via upload

--- requires/head_index.php
+++ requires/head_index.php
@@ -3,10 +3,11 @@
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="style/main.css">
-    <link rel="shortcut icon" type="image/png" href="/biblio_groupe3_etape4/img/icone/favicon.png"/>
+    <?php require "config.php"?>
+    <link rel="shortcut icon" type="image/png" href="<?php echo $CONFIG["root_path"] ?>img/icone/favicon.png"/>
     <title><?php echo $page_title ?></title>
   </head>
   <body>
   <header>
-<a href="/biblio_groupe3_etape4/index.php"><img src="/biblio_groupe3_etape4/img/logo.png" width="300" height="300" alt="Logo bibliotech"></a>
+<a href="/biblio_groupe3_etape4/index.php"><img src="<?php echo $CONFIG["root_path"] ?>/img/logo.png" width="300" height="300" alt="Logo bibliotech"></a>
  </header>
